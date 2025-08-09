c957482085d274d41835b312843d6678796b5def
Bit77 <daren@markandtraci.com>
2025-08-08 19:11:40 -0500

    Update at2.py

--- at2.py
+++ at2.py
@@ -1,1 +1,11 @@
+import numpy as np
+import time
 
+def stress_cpu(duration=30):
+    print("Stressing CPU...")
+    end = time.time() + duration
+    while time.time() < end:
+        np.linalg.eig(np.random.rand(300, 300))
+    print("CPU stress test complete.")
+
+stress_cpu()
